61b9d234cebf188d98273c031e19ea2f7e7e5599
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-22 10:53:40 +0530

    ip

--- roboshop-project/make-inventory.sh
+++ roboshop-project/make-inventory.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
->inventory
+>inventory     #just making empty inventory file
 for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis;do
      IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq \
      '.Reservations[].Instances[].PrivateIpAddress')
      if [ -n "${IP}" ];then
-      echo $IP component=${component} >> inventory
+       echo ${component}
+       echo $IP component=${component} >>inventory
      fi
 done
