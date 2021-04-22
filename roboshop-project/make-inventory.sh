b42e6766a512a840a8eb2ff3c71f7138f7ddd64c
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-22 10:58:11 +0530

    xargs

--- roboshop-project/make-inventory.sh
+++ roboshop-project/make-inventory.sh
@@ -3,7 +3,7 @@
 >inventory     #just making empty inventory file
 for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis;do
      IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq \
-     '.Reservations[].Instances[].PrivateIpAddress') | xargs
+     '.Reservations[].Instances[].PrivateIpAddress' |xargs)       #xargs used to remove""
      if [ -n "${IP}" ];then
        echo ${component}
        echo $IP component=${component} >>inventory
